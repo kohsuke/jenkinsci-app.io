127daadff49db4cdac2c8e83004c880ac67af187
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-04-24 14:54:17 -0700

    initial skeleton implementation

---
+++ src/main/java/org/jenkinsci/plugins/kickfolio/KickfolioCredentials.java
@@ -0,0 +1,37 @@
+package org.jenkinsci.plugins.kickfolio;
+
+import com.cloudbees.plugins.credentials.BaseCredentials;
+import com.cloudbees.plugins.credentials.CredentialsDescriptor;
+import hudson.Extension;
+import hudson.util.Secret;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class KickfolioCredentials extends BaseCredentials {
+    private final String username;
+    private final Secret apiKey;
+
+    @DataBoundConstructor
+    public KickfolioCredentials(String username, Secret apiKey) {
+        this.username = username;
+        this.apiKey = apiKey;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Secret getApiKey() {
+        return apiKey;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends CredentialsDescriptor {
+        @Override
+        public String getDisplayName() {
+            return "Kickfolio Credentials";
+        }
+    }
+}
---
+++ src/main/java/org/jenkinsci/plugins/kickfolio/KickfolioRecorder.java
@@ -0,0 +1,67 @@
+package org.jenkinsci.plugins.kickfolio;
+
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Publisher;
+import hudson.tasks.Recorder;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class KickfolioRecorder extends Recorder {
+    private String ipaFile;
+
+    @DataBoundConstructor
+    public KickfolioRecorder(String ipaFile) {
+        this.ipaFile = ipaFile;
+    }
+
+    public String getIpaFile() {
+        return ipaFile;
+    }
+
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.NONE;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+        listener.getLogger().println("Deploying to Kickfolio!");
+
+        // TODO:
+        FilePath ipa = build.getWorkspace().child(ipaFile);
+        // InputStream is = ipa.read();
+
+        List<KickfolioCredentials> c = CredentialsProvider.lookupCredentials(KickfolioCredentials.class, build.getProject());
+        KickfolioCredentials x = c.get(0);// TODO: check if there's a better way to do it
+
+        listener.getLogger().println(String.format("Using %s:%s", x.getUsername(), x.getApiKey()));
+
+
+        return true;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+        @Override
+        public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Deploy to Kickfoliio";
+        }
+    }
+}
