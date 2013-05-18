4886da10b1281cf38608a9eccfad38f20440c662
mqprichard <mqprichard@gmail>
2013-05-17 20:24:07 -0700

    Added KickfolioService.addVersion() and unit tests

--- src/main/java/org/jenkinsci/plugins/kickfolio/KickfolioService.java
+++ src/main/java/org/jenkinsci/plugins/kickfolio/KickfolioService.java
@@ -1,8 +1,13 @@
 package org.jenkinsci.plugins.kickfolio;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
@@ -10,17 +15,26 @@ import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.jenkinsci.plugins.kickfolio.model.KickfolioApp;
 import org.jenkinsci.plugins.kickfolio.model.KickfolioAppObject;
+import org.jenkinsci.plugins.kickfolio.model.KickfolioApps;
+import org.jenkinsci.plugins.kickfolio.model.KickfolioVersion;
+import org.jenkinsci.plugins.kickfolio.model.KickfolioVersionObject;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class KickfolioService {
-    public String createApp(String appName, String apiKey) throws Exception {
+
+    private final HttpHost httpHost = new HttpHost("kickfolio.com", 443,
+            "https");
+    private final HttpPost httpPost = new HttpPost("/api/apps");
+    private final HttpPost httpPostVersions = new HttpPost("/api/versions");
+    private final HttpGet httpGet = new HttpGet("/api/apps");
+    private final String kickfolio_v1 = "application/vnd.kickfolio.v1";
+
+    public KickfolioAppObject createApp(String appName, String apiKey) throws Exception {
         DefaultHttpClient httpClient = new DefaultHttpClient();
-        HttpHost httpHost = new HttpHost("kickfolio.com", 443, "https");
-        HttpPost httpPost = new HttpPost("/api/apps");
         ResponseHandler<String> handler = new BasicResponseHandler();
-        String result = null;
+        KickfolioAppObject theAppObject = new KickfolioAppObject();
 
         try {
             // Kickfolio Authorization and Content-Type headers
@@ -45,15 +59,17 @@ public class KickfolioService {
             StringEntity postBody = new StringEntity(gson.toJson(theApp),
                     ContentType.create("application/json", "UTF-8"));
             httpPost.setEntity(postBody);
+            System.out.println("Request: " + gson.toJson(theApp));
 
             // Call Kickfolio REST API to create the new app
             HttpResponse response = httpClient.execute(httpHost, httpPost);
             String jsonKickfolioApp = handler.handleResponse(response);
+            System.out.println("Response: " + jsonKickfolioApp);
 
             // Get JSON data from the HTTP Response
             KickfolioApp kickfolioApp = new Gson()
                     .fromJson(jsonKickfolioApp, KickfolioApp.class);
-            result = kickfolioApp.getApp().getId();
+            theAppObject = kickfolioApp.getApp();
 
         } catch (Exception e) {
             throw e;
@@ -63,6 +79,87 @@ public class KickfolioService {
             } catch (Exception ignore) {
             }
         }
-        return result;
+        return theAppObject;
+    }
+
+    public KickfolioAppObject findApp(String appName, String apiKey) throws Exception {
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        ResponseHandler<String> handler = new BasicResponseHandler();
+        KickfolioAppObject theApp = new KickfolioAppObject();
+
+        try {
+            // Kickfolio Authorization and Content-Type headers
+            String kickfolioAuth = "Basic " + apiKey;
+            httpGet.addHeader("Authorization", kickfolioAuth);
+            httpGet.addHeader("Accept", kickfolio_v1);
+
+            HttpResponse response = httpClient.execute(httpHost, httpGet);
+            String jsonKickfolioApps = handler.handleResponse(response);
+            System.out.println("Response: " + jsonKickfolioApps);
+
+            KickfolioApps kickfolioApps = new Gson()
+                    .fromJson(jsonKickfolioApps, KickfolioApps.class);
+            List<KickfolioAppObject> list = Arrays.asList(kickfolioApps
+                    .getApps());
+            Iterator<KickfolioAppObject> iterator = list.iterator();
+
+            boolean foundAppName = false;
+            while ((iterator.hasNext()) && (!foundAppName)) {
+                KickfolioAppObject thisApp = iterator.next();
+                if (thisApp.getName().equals(appName)) {
+                    theApp = thisApp;
+                }
+            }
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            try {
+                httpClient.getConnectionManager().shutdown();
+            } catch (Exception ignore) {
+            }
+        }
+        return theApp;
+    }
+
+    public KickfolioVersionObject addVersion(String appId, String urlUpload, String apiKey) throws Exception {
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        ResponseHandler<String> handler = new BasicResponseHandler();
+        KickfolioVersion newVersion = new KickfolioVersion();
+        KickfolioVersionObject versionObj = new KickfolioVersionObject();
+
+        try {
+            // Construct {"version": ... } message body
+            versionObj.setApp_id(appId);
+            versionObj.setBundle_url(urlUpload);
+            newVersion.setVersion(versionObj);
+            System.out.println("Request: " + new Gson().toJson(newVersion));
+
+            // Send new version REST call to Kickfolio
+            httpPostVersions.addHeader("Authorization", "Basic " + apiKey);
+            httpPostVersions.addHeader("Content-Type", "application/json");
+            httpPostVersions.addHeader("Accept", kickfolio_v1);
+            StringEntity reqBody = new StringEntity(
+                    new Gson().toJson(newVersion),
+                    ContentType.create("application/json", "UTF-8"));
+            httpPostVersions.setEntity(reqBody);
+            HttpResponse response = httpClient
+                    .execute(httpHost, httpPostVersions);
+
+            String jsonKickfolioVersion = handler.handleResponse(response);
+            System.out.println("Response: " + jsonKickfolioVersion);
+            newVersion = new Gson()
+                    .fromJson(jsonKickfolioVersion, KickfolioVersion.class);
+
+        } catch (Exception e) {
+            e.getStackTrace();
+            throw e;
+        } finally {
+            try {
+                httpClient.getConnectionManager().shutdown();
+            } catch (Exception ignore) {
+            }
+        }
+        return newVersion.getVersion();
     }
 }
--- src/test/java/org/jenkinsci/plugins/kickfolio/KickfolioServiceTest.java
+++ src/test/java/org/jenkinsci/plugins/kickfolio/KickfolioServiceTest.java
@@ -6,38 +6,122 @@ import static org.junit.Assert.fail;
 
 import java.util.UUID;
 
+import org.jenkinsci.plugins.kickfolio.model.KickfolioAppObject;
+import org.jenkinsci.plugins.kickfolio.model.KickfolioVersionObject;
 import org.junit.Test;
 
 public class KickfolioServiceTest {
     private final String apiKey = "UDFOZG9Gd21MdExIVjlwamtkamg=";
     private final String appName = "Stockfish";
     private final String badKey = "foo";
+    private final String badName = "foo";
+
+    private final String filePath = "/Users/markprichard/Documents/Kickfolio/StockFish.zip";
+    private final String fpApiKey = "AM5ozphAqSNKD6vhNfBivz";
+
+    // For chained tests
+    private KickfolioAppObject testAppObject = new KickfolioAppObject();
+    private KickfolioVersionObject testVersionObject = new KickfolioVersionObject();
 
     @Test
     public void createApp() {
-        String result = null;
+        KickfolioAppObject result = null;
         KickfolioService testService = new KickfolioService();
         try {
             result = testService.createApp(appName, apiKey);
+            testAppObject = result;
+            System.out
+                    .println(result.getName() + ", " + result.getPublic_key());
 
             // Quick test for valid UUID
-            UUID uid = UUID.fromString(result);
-            assertEquals(uid.toString().equals(result), true);
+            UUID uid = UUID.fromString(result.getId());
+            assertEquals(uid.toString().equals(result.getId()), true);
         } catch (Exception e) {
             fail(e.getMessage());
         }
     }
 
     @Test
-    public void testBadKey() {
-        String result = null;
+    public void createAppBadKey() {
+        KickfolioAppObject result = null;
         KickfolioService testService = new KickfolioService();
         try {
             result = testService.createApp(appName, badKey);
-            UUID uid = UUID.fromString(result);
-            assertEquals(uid.toString().equals(result), true);
+            System.out.println(result);
+
+            // Quick test for valid UUID
+            UUID uid = UUID.fromString(result.getId());
+            assertEquals(uid.toString().equals(result.getId()), true);
         } catch (Exception e) {
             assertTrue(e.getMessage(), true);
+        }
+    }
+
+    @Test
+    public void findApp() {
+        KickfolioAppObject result = null;
+        KickfolioService testService = new KickfolioService();
+        try {
+            result = testService.findApp(appName, apiKey);
+            System.out.println(result.getId());
+            System.out.println(result.getPublic_key());
+
+            // Quick test for valid UUID
+            UUID uid = UUID.fromString(result.getId());
+            assertEquals(uid.toString().equals(result.getId()), true);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void findAppNotFound() {
+        KickfolioAppObject result = null;
+        KickfolioService testService = new KickfolioService();
+        try {
+            result = testService.findApp(badName, apiKey);
+            assertEquals(result.getId(), null);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void findAppBadKey() {
+        KickfolioAppObject result = null;
+        KickfolioService testService = new KickfolioService();
+        try {
+            result = testService.findApp(appName, badKey);
+            assertEquals(result.getId(), null);
+        } catch (Exception e) {
+            assertTrue(e.getMessage(), true);
+        }
+    }
+
+    @Test
+    public void addVersion() {
+        // KickfolioVersionObject result = null;
+        KickfolioService testService = new KickfolioService();
+        try {
+            // Upload new bits via Filepicker
+            FilepickerService filepicker = new FilepickerService();
+            String fileUrl = filepicker.getUploadURL(filePath, fpApiKey);
+
+            // Create a new Kickfolio app
+            KickfolioAppObject testAppObject = testService
+                    .createApp(appName, apiKey);
+
+            // Add a new version
+            KickfolioVersionObject testVersionObject = testService
+                    .addVersion(testAppObject.getId(), fileUrl, apiKey);
+
+            // Get app info and check for new version id
+            testAppObject = testService.findApp(appName, apiKey);
+            assertEquals(testAppObject.getId(), testVersionObject.getApp_id());
+            assertEquals(testAppObject.getVersion_ids()[0], testVersionObject.getId());
+
+        } catch (Exception e) {
+            fail(e.getMessage());
         }
     }
 }
